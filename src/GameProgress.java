467e72856dbc17a4bd5c322c0dff4026273503a6
Котегова Мария <m.kotegowa@yandex.ru>
2022-07-11 17:43:27 +0300

    1 и 2 задание создание и сохранение игры

---
+++ src/GameProgress.java
@@ -0,0 +1,28 @@
+import java.io.Serializable;
+
+public class GameProgress implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private int health;
+    private int weapons;
+    private int lvl;
+    private double distance;
+
+    public GameProgress(int health, int weapons, int lvl, double distance) {
+        this.health = health; //  здоровье
+        this.weapons = weapons;  // оружее
+        this.lvl = lvl;   //уровень
+        this.distance = distance;  // дистанция
+    }
+
+    @Override
+    public String toString() {
+        return "GameProgress{" +
+                "health=" + health +
+                ", weapons=" + weapons +
+                ", lvl=" + lvl +
+                ", distance=" + distance +
+                '}';
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,108 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class Main {
+    public static StringBuilder log = new StringBuilder();
+    public static Date date = new Date();
+
+    public static void main(String[] args) {
+        newDir("C:\\Новая папка (2)\\Games", "Games");
+        newDir("C:\\Новая папка (2)\\Games\\src", "src");
+        newDir("C:\\Новая папка (2)\\Games\\res", "res");
+        newDir("C:\\Новая папка (2)\\Games\\savegames", "savegames");
+        newDir("C:\\Новая папка (2)\\Games\\temp", "temp");
+        newDir("C:\\Новая папка (2)\\Games\\src\\main", "main");
+        newDir("C:\\Новая папка (2)\\Games\\src\\test", "test");
+
+        newFile("C:\\Новая папка (2)\\Games\\src\\main\\Main.java", "Main.java");
+        newFile("C:\\Новая папка (2)\\Games\\src\\main\\Utils.java", "Utils.java");
+
+        newDir("C:\\Новая папка (2)\\Games\\res\\drawables", "drawables");
+        newDir("C:\\Новая папка (2)\\Games\\res\\vectors", "vectors");
+        newDir("C:\\Новая папка (2)\\Games\\res\\icons", "icons");
+
+        newFile("C:\\Новая папка (2)\\Games\\temp\\temp.txt", "temp.txt");
+
+        //запись
+        String text = log.toString();
+        try (FileOutputStream fos = new FileOutputStream("C:\\Новая папка (2)\\Games\\temp\\temp.txt", true)) { //записать что то в выходной поток
+            byte[] bytes = text.getBytes(); // получаем из строки массив байт
+            fos.write(bytes, 0, bytes.length); // записать байты в файл с 0 позиц до последней
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        GameProgress user1 = new GameProgress(100, 30, 1, 200.76);
+        GameProgress user2 = new GameProgress(58, 50, 30, 10000.55);
+        GameProgress user3 = new GameProgress(70, 80, 40, 500000.77);
+
+        saveGame("C:\\Новая папка (2)\\Games\\savegames\\save1.dat", user1);
+        saveGame("C:\\Новая папка (2)\\Games\\savegames\\save2.dat", user2);
+        saveGame("C:\\Новая папка (2)\\Games\\savegames\\save3.dat", user3);
+
+        List<String> list = Arrays.asList("C:\\Новая папка (2)\\Games\\savegames\\save1.dat",
+                "C:\\Новая папка (2)\\Games\\savegames\\save2.dat",
+                "C:\\Новая папка (2)\\Games\\savegames\\save3.dat");
+
+        zipFiles("C:\\Новая папка (2)\\Games\\savegames\\zip_output.zip", list);
+
+        fileDelete("C:\\Новая папка (2)\\Games\\savegames\\save1.dat");
+        fileDelete("C:\\Новая папка (2)\\Games\\savegames\\save2.dat");
+        fileDelete("C:\\Новая папка (2)\\Games\\savegames\\save3.dat");
+    }
+
+    public static void fileDelete(String path) {
+        File file = new File(path);
+        file.delete();
+    }
+
+    public static void saveGame(String text, GameProgress user) {
+        try (FileOutputStream fos = new FileOutputStream(text);
+             ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+            oos.writeObject(user);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public static void zipFiles(String pathZip, List<String> list) {
+        for (int i = 0; i < list.size(); i++) {
+            try (ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(pathZip));
+                 FileInputStream fis = new FileInputStream(list.get(i))) { // этот файл запишем в архив
+                ZipEntry entry = new ZipEntry("paked_notes.txt");   // записываем имя нового файла в архиве
+                zout.putNextEntry(entry); // говорим что сохраняем новый файл
+
+                byte[] buffer = new byte[fis.available()]; // Буферт будет размера этого файла
+                fis.read(buffer);
+
+                zout.write(buffer);
+                zout.closeEntry();  //  закрывает операцию
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+            }
+
+        }
+    }
+
+
+    public static void newDir(String text, String name) {
+        File dir = new File(text);
+        if (dir.mkdir())                          // создастся папка
+            log.append("Папка " + name + " создана, время создания: " + date.toString() + "\n" + "путь к папке: " + text + "\n");
+    }
+
+    public static void newFile(String text, String name) {
+        File file = new File(text); // создали файл в папке
+        try {
+            if (file.createNewFile())     // если есть папка до создать файл
+                log.append("Файл " + name + " создан, время создания: " + date.toString() + "\n" + "путь к файлу: " + text + "\n");
+        } catch (IOException e) {    // если нет папки то создать исключение
+            System.out.println(e.getMessage());
+        }
+
+    }
+}
